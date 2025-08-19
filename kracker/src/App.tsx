0092ab2a394e39d8c9063689695a755bebf706f4
apkool12 <apkool12@naver.com>
2025-08-19 22:17:36 +0900

    [feat] - create nickname modal

--- kracker/src/App.tsx
+++ kracker/src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import NicknameModal from "../src/components/modals/NicknameModal";
 import {
   BrowserRouter,
   Routes,
@@ -15,9 +16,25 @@ import Game from "./components/RoundsGame";
 import { BgmProvider } from "./providers/BgmProvider";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  // 페이지 로드 시 닉네임 확인
+  useEffect(() => {
+    const savedNickname = localStorage.getItem("userNickname");
+    setShowModal(true);
+  }, []);
   return (
     <BrowserRouter>
       <BgmProvider>
+        <div>
+          <NicknameModal
+            isOpen={showModal}
+            onSubmit={(nickname) => {
+              console.log("닉네임:", nickname);
+              setShowModal(false);
+            }}
+          />
+        </div>
         <Routes>
           <Route path="/test" element={<ModalTest />} />
           <Route path="/" element={<Home />} />
